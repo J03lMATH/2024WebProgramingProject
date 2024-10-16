2347fc54789e79942f8f344475df9a5d5fe89377
J03lMATH <157427495+J03lMATH@users.noreply.github.com>
2024-10-15 23:54:12 -0400

    Display Products for Purchase

--- Client/src/components/NavBar.vue
+++ Client/src/components/NavBar.vue
@@ -36,15 +36,17 @@ const isOpen = ref(false)
       <div class="navbar-menu" :class="{ 'is-active': isOpen }">
         <div class="navbar-start">
           <RouterLink to="/" class="navbar-item">Home</RouterLink>
-          <RouterLink to="/about" class="navbar-item">About</RouterLink>
+          <RouterLink to="/product" class="navbar-item">Shop</RouterLink>
 
           <div class="navbar-item has-dropdown is-hoverable">
             <a class="navbar-link"> More </a>
 
             <div class="navbar-dropdown">
-              <a class="navbar-item"> About </a>
-              <a class="navbar-item is-selected"> Jobs </a>
-              <a class="navbar-item"> Contact </a>
+              <RouterLink to="/about" class="navbar-item"> About </RouterLink>
+              <RouterLink to="/jobs" class="navbar-item"> Jobs </RouterLink>
+              <RouterLink to="/contact" class="navbar-item">
+                Contact
+              </RouterLink>
               <hr class="navbar-divider" />
               <a class="navbar-item"> Report an issue </a>
             </div>
@@ -65,4 +67,9 @@ const isOpen = ref(false)
     </div>
   </nav>
 </template>
-<style scoped></style>
+<style scoped>
+.router-link-active {
+  font-weight: bold;
+  border-bottom: 2px solid blue;
+}
+</style>
