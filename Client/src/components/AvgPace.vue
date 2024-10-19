29cd4158099ae59233690c58671575bbedf150c9
J03lMATH <157427495+J03lMATH@users.noreply.github.com>
2024-10-19 18:04:58 -0400

    adding more compents and pages

---
+++ Client/src/components/AvgPace.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="container">
+    <h3 id="pace" class="subtitle is-3">NaN mph</h3>
+    <p>Avg Pace</p>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  box-shadow: drop-shadow(0 0 10px rgba(0, 0, 0, 0.8));
+  padding: 1rem;
+  text-align: center;
+}
+</style>
