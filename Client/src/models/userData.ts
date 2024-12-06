abce29cf27acc1bfbff08cad6a6c529c149f311e
J03lMATH <157427495+J03lMATH@users.noreply.github.com>
2024-12-06 14:15:02 -0500

    updating some explanations

--- Client/src/models/userData.ts
+++ Client/src/models/userData.ts
@@ -6,6 +6,7 @@ export interface UserData {
   infos: Info[]
 }
 
+//Commands that help to manage the users
 export function sortUsersInfoByDateForFriends(
   users: User[],
   ascending: boolean = true,
